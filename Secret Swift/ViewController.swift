6ef6eb3b8501008c4fb3a2f0f897f828e0ddba88
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-02-11 21:26:42 +0300

    Creating a basic text editor

--- Secret Swift/ViewController.swift
+++ Secret Swift/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  //MARK: - Outlats
+  @IBOutlet var secretText: UITextView!
+
+  //MARK: - UIView Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    setNotifications()
+
   }
 
+  //MARK: - Methods
+  private func setNotifications() {
+    let notificationCenter = NotificationCenter.default
+    notificationCenter.addObserver(self, selector: #selector(adjustForKeyBoard), name: UIResponder.keyboardWillHideNotification, object: nil)
+    notificationCenter.addObserver(self, selector: #selector(adjustForKeyBoard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+  }
 
+  @objc private func adjustForKeyBoard(notification: Notification) {
+    guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+    let keyboardScreenEnd = keyboardValue.cgRectValue
+    let keyboardViewEndFrame = view.convert(keyboardScreenEnd, from: view.window)
+    if notification.name == UIResponder.keyboardWillHideNotification {
+      secretText.contentInset = .zero
+    } else {
+      secretText.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+    }
+    secretText.scrollIndicatorInsets = secretText.contentInset
+    let selectedRange = secretText.selectedRange
+    secretText.scrollRangeToVisible(selectedRange)
+  }
+
+  //MARK: - Actions
+  @IBAction func authenticateTapped(_ sender: Any) {
+  }
 }
 
