20f5fa046eef185be3bd81777766793153226f1f
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-02-13 22:50:59 +0300

    Wrap up + screenshots

--- Secret Swift/ViewController.swift
+++ Secret Swift/ViewController.swift
@@ -18,6 +18,9 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     setNotifications()
     title = "Nothing to see here!"
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(saveSecretMessage))
+    navigationItem.rightBarButtonItem?.isEnabled = false
+    navigationItem.rightBarButtonItem?.tintColor = .clear
   }
 
   //MARK: - Methods
@@ -45,6 +48,8 @@ class ViewController: UIViewController {
   private func unlockSecretText() {
     secretText.isHidden = false
     title = "Secret stuff!"
+    navigationItem.rightBarButtonItem?.isEnabled = true
+    navigationItem.rightBarButtonItem?.tintColor = nil
     if let text = KeychainWrapper.standard.string(forKey: "SecretMessage") {
       secretText.text = text
     }
@@ -55,7 +60,60 @@ class ViewController: UIViewController {
     KeychainWrapper.standard.set(secretText.text, forKey: "SecretMessage")
     secretText.resignFirstResponder()
     secretText.isHidden = true
+    navigationItem.rightBarButtonItem?.isEnabled = false
+    navigationItem.rightBarButtonItem?.tintColor = .clear
     title = "Nothing to see here!"
+  }
+
+  private func loginWithPassword() {
+    let alert = UIAlertController(title: "Enter your login and password, please", message: nil, preferredStyle: .alert)
+    alert.addTextField { login in
+      login.placeholder = "Enter your login here"
+    }
+    alert.addTextField { password in
+      password.placeholder = "Enter your password where"
+      password.isSecureTextEntry = true
+    }
+    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+    alert.addAction(UIAlertAction(title: "Check", style: .default, handler: { [weak self] _ in
+      if let login = alert.textFields?[0].text {
+        if let storedLogin = KeychainWrapper.standard.string(forKey: "Login") {
+          if login == storedLogin {
+            if let password = alert.textFields?[1].text {
+              if let storedPassword = KeychainWrapper.standard.string(forKey: "Password") {
+                if password == storedPassword {
+                  self?.unlockSecretText()
+                }
+              }
+            }
+          }
+        } else {
+          self?.nonStoredPassData()
+        }
+      }
+    }))
+    present( alert, animated: true)
+  }
+
+  private func nonStoredPassData() {
+    let alert = UIAlertController(title: "You haven't set your username and password yet.", message: "Please enter your username and password for further authentication.", preferredStyle: .alert)
+    alert.addTextField { login in
+      login.placeholder = "Your Login..."
+    }
+    alert.addTextField { password in
+      password.placeholder = "Your password..."
+      password.isSecureTextEntry = true
+    }
+    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak self] _ in
+      guard let login = alert.textFields?[0].text else { return }
+      guard let password = alert.textFields?[1].text else { return }
+      if !login.isEmpty && !password.isEmpty {
+        KeychainWrapper.standard.set(login, forKey: "Login")
+        KeychainWrapper.standard.set(password, forKey: "Password")
+      }
+    }))
+    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+    present(alert, animated: true)
   }
 
   //MARK: - Actions
@@ -69,8 +127,11 @@ class ViewController: UIViewController {
           if success {
             self?.unlockSecretText()
           } else {
-            let alert = UIAlertController(title: "Authentication failed!", message: "You can't be verified. Pleastry again.", preferredStyle: .alert)
+            let alert = UIAlertController(title: "Authentication failed!", message: "You can't be verified. Please, try again.", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default))
+            alert.addAction(UIAlertAction(title: "Use login and password", style: .default, handler: { [weak self] _ in
+              self?.loginWithPassword()
+            }))
             self?.present(alert, animated: true)
           }
         }
